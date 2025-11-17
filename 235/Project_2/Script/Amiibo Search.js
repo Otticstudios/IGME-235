15281e5ce41da7409326c603afac59e8064f8adc
Otticstudios <76819877+Otticstudios@users.noreply.github.com>
2025-11-17 08:46:27 -0500

    stuff

---
+++ 235/Project_2/Script/Amiibo Search.js
@@ -0,0 +1,89 @@
+// 1
+  	window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
+	
+	// 2
+	let displayTerm = "";
+// 3 Function to get search terms
+function searchButtonClicked()
+{
+    //1 Amiibo API URL
+    const amiiboURL= "https://www.amiiboapi.com/api/amiibo/";
+    //2 Build up URL string
+    let URL= amiiboURL;
+    //3 Grab Users chosen search Filter (Name, Game Series, Amiibo Series, Character Name, Type)
+    let filter= document.querySelector("#filter").value;
+    URL+="?"+filter;
+    //4 Get SearchTerm
+	let term= document.querySelector("#searchterm").value;
+	displayTerm=term;
+	//5 trim Searchterm
+	term=term.trim();
+	//6 Encode Spaces
+	term= encodeURIComponent(term);
+	//7 if no Search terms, return
+	if(term.length<1) return;
+    //8 Add Search term to URL
+    URL+="="+term;
+ //9 update UI
+	document.querySelector("#status").innerHTML="<b>Searching for '"+ displayTerm+"'</b>";
+		//10 Log URL
+		console.log(URL);
+		// 12 Request data!
+		getData(URL);
+
+}
+//4
+function getData(url)
+	{
+		//1
+		let xhr= new XMLHttpRequest();
+		//2
+		xhr.onload= dataLoaded;
+		//3
+		xhr.onerror= dataError;
+		//4
+		xhr.open("GET",url);
+		xhr.send();
+		
+	}
+//Callback functions
+	function dataLoaded(e)
+	{
+		let xhr=e.target;
+		console.log(xhr.responseText);
+		let  obj= JSON.parse(xhr.responseText);
+		if(!obj.amiibo||obj.amiibo.length==0){
+			document.querySelector("#status").innerHTML = "<b>No results found for '"+displayTerm+"'</b>";
+			return;
+		}
+		let results = obj.amiibo;
+		console.log("results.legnth = "+results.length);
+		let bigString ="<p><i>Here are "+results.length+" results for '"+displayTerm+"'</i></p>";
+        //Loop
+		for(let i=0;i<results.length;i++)
+            {
+                let result=results[i];
+                //Grab Results
+                let name= result.name;
+                let game= result.gameSeries;
+                let series= result.amiiboSeries;
+                let type= result.type;
+                let character= result.character;
+                let imageURL= result.image;
+                if(!imageURL) imageURL="images/no-image-found.png";
+                let line=`<div class="result">
+                <img src="${imageURL}" width="120">
+                <h3>${name}</h3>
+                <p>Character: ${character}</p>
+                <p>Game Series: ${game}</p>
+                <p>Amiibo Series: ${series}</p>
+                <p>Type: ${type}</p> </div>`;
+                bigString+=line;
+            }
+        document.querySelector('#content').innerHTML=bigString;
+		document.querySelector('#status').innerHTML="<b>Success!</b>";
+    }
+function dataError(e)
+	{
+		console.Log("An error occured");
+	}
